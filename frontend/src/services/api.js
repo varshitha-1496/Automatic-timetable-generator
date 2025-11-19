62514b201ab77c8fff2c45e34931e5daf4a5bb4f
varshitha-1496 <reddyvarshitha72@gmail.com>
2025-11-19 21:48:23 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://automatic-timetable-generator-backend.onrender.com/api',
   withCredentials: true
 })
 
